4a15d90143e8d8a53c8997f79a5328a4dc4d0bfe
c_door In <55236660+git-tutorial-cmd@users.noreply.github.com>
2021-12-02 23:00:32 +0300

    Move EnvVars to main()

--- main.py
+++ main.py
@@ -56,15 +56,21 @@ def get_language_stat(all_salaries, found):
     }
 
 
-def get_vacancies_statistic_hh(env, languages):
+def get_vacancies_statistic_hh(
+            languages,
+            hh_professional_roles,
+            hh_parent_area,
+            hh_area,
+            hh_period
+        ):
     vacancies_statistic = {}
     for language in languages:
         vacancies, found = parse_vacancies_hh(
             language,
-            professional_roles=env('HH_PROFESSIONAL_ROLE', '96'),
-            parent_area=env('HH_PARENT_AREA', '113'),
-            area=env('HH_AREA', '1'),
-            period=env('HH_SEARCHING_PERIOD', '30'),
+            professional_roles=hh_professional_roles,
+            parent_area=hh_parent_area,
+            area=hh_area,
+            period=hh_period,
         )
         all_salaries = fetch_all_rub_salary(vacancies, predict_rub_salary_hh)
         if not all_salaries:
@@ -73,14 +79,19 @@ def get_vacancies_statistic_hh(env, languages):
     return vacancies_statistic
 
 
-def get_vacancies_statistic_sj(env, languages):
+def get_vacancies_statistic_sj(
+            languages,
+            sj_catalogues,
+            sj_town,
+            sj_secret_key
+        ):
     vacancies_statistic = {}
     for language in languages:
         vacancies, found = parse_sj_vacancies(
             language,
-            catalogues=env('SJ_CATALOGUES_CODE', '48'),
-            town=env('SJ_TOWN', '113'),
-            superjob_secret_key = env('SUPERJOB_SECRET_KEY'),
+            catalogues=sj_catalogues,
+            town=sj_town,
+            superjob_secret_key=sj_secret_key,
         )
         all_salaries = fetch_all_rub_salary(vacancies, predict_rub_salary_sj)
         if not all_salaries:
@@ -135,8 +146,28 @@ def main():
         ],
     )
 
-    vacancies_statistic_hh = get_vacancies_statistic_hh(env, languages)
-    vacancies_statistic_sj = get_vacancies_statistic_sj(env, languages)
+    hh_professional_roles = env('HH_PROFESSIONAL_ROLE', '96')
+    hh_parent_area = env('HH_PARENT_AREA', '113')
+    hh_area = env('HH_AREA', '1')
+    hh_period = env('HH_SEARCHING_PERIOD', '30')
+
+    sj_catalogues = env('SJ_CATALOGUES_CODE', '48')
+    sj_town = env('SJ_TOWN', '113')
+    sj_secret_key = env('SUPERJOB_SECRET_KEY')
+
+    vacancies_statistic_hh = get_vacancies_statistic_hh(
+        languages,
+        hh_professional_roles,
+        hh_parent_area,
+        hh_area,
+        hh_period,
+    )
+    vacancies_statistic_sj = get_vacancies_statistic_sj(
+        languages,
+        sj_catalogues,
+        sj_town,
+        sj_secret_key,
+    )
 
     print(draw_terminaltable(vacancies_statistic_hh, 'HeadHunter Moscow'))
     print(draw_terminaltable(vacancies_statistic_sj, 'SuperJob Moscow'))
